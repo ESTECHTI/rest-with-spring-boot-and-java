d039ca955b8e0b50beb6172a4cedce7c84c866be
Elisandro Silva de Oliveira <e.s.tech@outlook.com>
2025-09-04 16:04:02 -0300

    Revert "Migrations with flyway"

--- 09_WorkingWithDTOPattern/rest-with-spring-boot-and-java/src/main/java/br/com/estech/repository/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.estech.repository;
-
-import br.com.estech.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {}
